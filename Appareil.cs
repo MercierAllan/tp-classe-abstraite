3ff1d5e24303a282df6bebc40e03ddb4b2b1b7a8
Mercier Allan <Allan.mercier@edouardgand.com>
2026-03-31 09:15:32 +0200

    Création de la classe abstrait

---
+++ Appareil.cs
@@ -0,0 +1,34 @@
+abstract class Appareil
+{
+    protected string marque;
+    protected string modele;
+    protected DateTime dateFabrication;
+
+    public Appareil(){}
+
+    public Appareil(string marque, string modele, DateTime dateFabrication)
+    {
+        this.marque = marque;
+        this.modele = modele;
+        this.dateFabrication = dateFabrication;
+    }
+
+    public void Allumer()
+    {
+        Console.WriteLine("L'appareil s'allume");
+    }
+
+    public void AfficherType()
+    {
+        Console.WriteLine("Je suis un appareil");
+    }
+
+    public void AfficherGarentie()
+    {
+        Console.WriteLine("Garantie standard de l'appareil");
+    }
+
+    public abstract void AfficherInfos();
+
+    public abstract void CalculerAnciennete();
+}
